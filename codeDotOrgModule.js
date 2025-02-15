6fa5b6b7612f3ebc840ea4c6e6e2429c06d0b127
Ruchir <ruchirkafle@gmail.com>
2025-02-14 22:44:14 -0500

    exported function bases

---
+++ codeDotOrgModule.js
@@ -0,0 +1,67 @@
+export function onEvent(id, type, callback) {
+
+}
+
+export function getText(id) {
+
+}
+
+export function setText(id, text) {
+
+}
+
+export function getNumber(id) {
+
+}
+
+
+export function playSound(url, loop) {
+
+}
+
+export function stopSound(url) {
+
+}
+
+export function showElement(id) {
+
+}
+
+export function hideElement(id) {
+
+}
+
+export function setProperty(id, property, value) {
+
+}
+
+export function getProperty(id, property) {
+
+}
+
+export function setScreen(screenId) {
+
+}
+
+
+export function insertItem(list, index, item) {
+
+}
+
+export function appendItem(list, index, item) {
+
+}
+
+export function removeItem(list, index, item) {
+
+}
+
+
+export function getColumn(table, column) {
+
+}
+
+
+export function randomNumber(min, max) {
+
+}
